6af6d6d56bf1be6f96ef9067b5a6ecb8fcbf11d0
Bob <bobt4lsu@att.net>
2019-05-17 17:10:16 -0500

    Node 1-afternoon complete

---
+++ server/getProducts.js
@@ -0,0 +1,12 @@
+const products = require("../products.json");
+
+const getProducts = (request, response) => {
+
+if (request.query.price) {
+    const items = products.filter(num => num.price >= parseInt(request.query.price));
+    return response.status(200).send(items);
+}
+    response.status(200).send(products);
+}
+
+module.exports= getProducts;
